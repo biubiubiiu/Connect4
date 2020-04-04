8974c7c6c4acc1110e572a4ba477d5823543eece
raymond <1049204910@qq.com>
2020-04-04 22:29:43 +0800

    添加倒计时组件

---
+++ Connect4/src/ui/components/CountdownTimer.java
@@ -0,0 +1,77 @@
+package ui.components;//
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+class CountdownTimer extends JPanel {
+    private static final int COUNTDOWN = 15000; // 15000ms
+    private static final int ONE_SEC = 1000;
+    private int current;
+
+    private final JLabel display;
+    private final JButton start;
+    private final Timer timer;
+
+    private timeoutCallback callback;
+
+    CountdownTimer() {
+        super();
+        start = new JButton("Start");
+        display = new JLabel(timeFormat(COUNTDOWN));
+        timer = new Timer(ONE_SEC, new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                current -= ONE_SEC;
+                display.setText(timeFormat(current));
+                if (current <= 0) {
+                    timer.stop();
+                    callback.run();
+                }
+            }
+        });
+        timer.setRepeats(true);
+
+        start.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                startCountdown();
+            }
+        });
+
+        this.add(start);
+        this.add(display);
+    }
+
+    private String timeFormat(int time) {
+        return (time / 1000 + "s");
+    }
+
+    private void startCountdown() {
+        current = COUNTDOWN; // 重新装入倒计时
+        display.setText(timeFormat(current));
+        timer.start();
+    }
+
+    private void stopCountdown() {
+        display.setText(timeFormat(current));
+        timer.stop();
+    }
+
+    public void setTimeoutCallback(timeoutCallback callback) {
+        this.callback = callback;
+    }
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("test");
+        frame.setLayout(new FlowLayout());
+        frame.setSize(220, 120);
+        frame.add(new CountdownTimer());
+        frame.setVisible(true);
+    }
+}
+
+interface timeoutCallback {
+    public void run();
+}
